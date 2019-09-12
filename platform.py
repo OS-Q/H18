d3b4e99d886d289e5c6aec8a3c342074f7b8bce5
qitas <qitas@qitas.cn>
2019-09-12 11:06:14 +0800

    fix

--- platform.py
+++ platform.py
@@ -7,7 +7,7 @@ class H18Platform(PlatformBase):
     @property
     def packages(self):
         packages = PlatformBase.packages.fget(self)
-        if (get_systype() == "H18" and
+        if (get_systype() == "linux_x86_64" and
                 "toolchain-gcclinux64" in packages):
             del packages['toolchain-gcclinux64']
         return packages
